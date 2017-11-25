c4ebfa7031e371e6f903bb94cc5ed9375b0173b3
anssi <anssi.moisio@aalto.fi>
2017-11-25 17:28:54 +0200

    Added a sceleton for Qlearning implementation.

---
+++ src/preQlearning.hpp
@@ -0,0 +1,115 @@
+functions for Q-learner
+
+
+class Actor(){
+
+}
+
+
+class Sensor(){
+
+}
+
+typedef float QvalueType;
+typedef int stateType;
+typedef int actionType;
+typedef int rewardType;
+
+class Qtable(std::vector<Actor> actors, std::vector<Sensor> sensors){
+    /* vector of state-vectors that contain actions for that state
+    */
+public:
+    Qtable()
+    /* qValues is initialized from actors and sensors */
+
+    QvalueType& getQvalue(stateType state, actionType action){
+    /*
+    */
+    return qValues[state][action];
+
+
+    actionType& getBestAction(stateType state);
+
+
+    actionType& getRandomAction(stateType state);
+
+
+    qValueType& getOptimalFutureQvalue(stateType state);
+    /* Compares actions
+     * and returns the largest Q-value
+     */
+
+    void updateQvalue(stateType state, actionType action, qValueType qValue);
+
+
+private:
+    std::vector<std::vector<QvalueType>> qValues; // qValues[state][action]
+
+}
+
+class
+
+class Agent(std::vector<Actor> actors, std::vector<Sensor> sensors, Qtable qtable)
+{
+public:
+    actionType& chooseAction(state);
+    // chooses the best or a random action
+
+
+    rewardType& calcReward(std::vector<ResponsePacket>);
+    //
+
+    updateQtable(state, action, nextState)
+                        maxQ = getMaxQ(nextState)
+                        reward = getReward()
+                        Q[state][action] = (1-alpha)*Q[state][action] + alpha *
+                                                        (reward + gamma*maxQ)
+
+                        return void
+
+private:
+    double discountFactor; // range 0...1, e.g. 0.9, increase
+    double learningRate; // range: 0...1, e.g. 0.1
+    double explorationFactor; // range: 0...1, e.g. 0.5, decrease
+
+    int state;
+
+}
+
+
+class AgentManager(){
+public:
+    agentThreadFunction(&Qvalues, ) //all parameter for agent and simulation classes
+    /* The function used by thread to run the learning and simulation of one
+       agent
+     */
+
+    initRun(simulationFileName, qvaluesFilename){
+    // reads files and creates threads that contain an agent and a simulation
+    }
+
+    evolveAgents{
+    /* Copies the qValues of the best agent to other agents from the previous
+     * generation and modifies the parameters in the Q-learning function.
+     */
+    }
+
+    pauseSimulation()
+    // pauses all the threads
+
+    saveQvalues()
+    // makes the first agent save its Q-values to file
+
+    endSimulation()
+    // terminate threads
+
+private:
+    std::vector<thread_type> agentThreads;
+}
+
+
+
+
+
+
+
