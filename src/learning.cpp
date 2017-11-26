68ba21339aba3aa9fbe9052c58764e001280661e
miitcher <mikael.gron@hotmail.com>
2017-11-26 22:35:11 +0200

    Implemented and made tests for the classes used for actors and sensors. For now the sensors and anctors only use angles. Other interactors can be later added, if we want.

--- src/learning.cpp
+++ src/learning.cpp
@@ -1,8 +1,24 @@
 #include "learning.hpp"
 #include <iostream>
+#include <vector>
 
-Actor::Actor() {}
+Actor::Actor(int ID, std::string const& description,
+    int quantizationSteps, float minAngle, float maxAngle,
+    std::vector<ActionType> actions)
+    : Interactor(ID, description, quantizationSteps, minAngle, maxAngle),
+    actions(actions) {}
 
-int Actor::returnTwo() {
-    return 2;
-}
+Sensor::Sensor(int ID, std::string const& description,
+    int quantizationSteps, float minAngle, float maxAngle)
+    : Interactor(ID, description, quantizationSteps, minAngle, maxAngle) {}
+
+JointActor::JointActor(int ID, int quantizationSteps,
+    float minAngle, float maxAngle)
+    : Actor(ID, "actor of rotating 2D joint",
+    quantizationSteps, minAngle, maxAngle,
+    {Still, Counterclockwise, Clockwise}) {}
+
+JointSensor::JointSensor(int ID, int quantizationSteps,
+    float minAngle, float maxAngle)
+    : Sensor(ID, "sensor of 2D joints angle",
+    quantizationSteps, minAngle, maxAngle) {}
