b96c21f8cde4cfa4a6f1bce817ebb91598725dff
Laaden <lassi.knuuttila@aalto.fi>
2017-11-30 15:44:13 +0200

    Added a rectangle to the world.

--- src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
+++ src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
@@ -15,6 +15,22 @@
 				shape.Set(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
 				ground->CreateFixture(&shape, 0.0f);
 			}
+			b2Body* crawler = NULL;
+			{
+				b2BodyDef myBodyDef;
+  				myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
+  				myBodyDef.position.Set(0, 1); //set the starting position
+  				myBodyDef.angle = 0; //set the starting angle
+				crawler = m_world->CreateBody(&myBodyDef);
+				
+				b2PolygonShape boxShape;
+  				boxShape.SetAsBox(2,1);
+  
+  				b2FixtureDef boxFixtureDef;
+  				boxFixtureDef.shape = &boxShape;
+  				boxFixtureDef.density = 1;
+  				crawler->CreateFixture(&boxFixtureDef);
+			}
    		}
 		
         void Step(Settings* settings)
