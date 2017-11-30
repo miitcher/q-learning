94eaea8fc5481851c3629be6f7022ba420fa439d
Laaden <lassi.knuuttila@aalto.fi>
2017-11-30 15:07:53 +0200

    Added ground object to the Testv.h

--- src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
+++ src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
@@ -4,8 +4,19 @@
   class Testv : public Test
     {
         public:
-        Testv() { } //do nothing, no scene yet
-    
+        Testv() 
+		{
+			b2Body* ground = NULL;
+			{
+				b2BodyDef bd;
+				ground = m_world->CreateBody(&bd);
+
+				b2EdgeShape shape;
+				shape.Set(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
+				ground->CreateFixture(&shape, 0.0f);
+			}
+   		}
+		
         void Step(Settings* settings)
         {
             //run the default physics and rendering
