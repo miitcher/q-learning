28eea69e9dbf3230fd4a4652b26e2e9b543bc7ad
Visa Koski <visa.koski@aalto.fi>
2017-11-30 16:45:34 +0200

    added basic joint

--- src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
+++ src/Box2D_v2.3.0/Box2D/Testbed/Tests/Testv.h
@@ -28,9 +28,36 @@
   
   				b2FixtureDef boxFixtureDef;
   				boxFixtureDef.shape = &boxShape;
-  				boxFixtureDef.density = 1;
+  				boxFixtureDef.density = 5;
+				boxFixtureDef.friction = 10;
   				crawler->CreateFixture(&boxFixtureDef);
+
+				// create arm1
+				b2Body* arm1 = NULL;
+				myBodyDef.type = b2_dynamicBody; //this will be a dynamic body
+  				myBodyDef.position.Set(2, 1); //set the starting position
+  				myBodyDef.angle = 0; //set the starting angle
+				arm1 = m_world->CreateBody(&myBodyDef);
+				
+  				boxShape.SetAsBox(2,0.1);
+  
+  				boxFixtureDef.shape = &boxShape;
+  				boxFixtureDef.density = 1;
+  				arm1->CreateFixture(&boxFixtureDef);
+
+				// set joint
+				b2RevoluteJointDef revoluteJointDef;
+ 				revoluteJointDef.bodyA = crawler;
+				revoluteJointDef.bodyB = arm1;
+				revoluteJointDef.collideConnected = false;
+				revoluteJointDef.localAnchorA.Set(1,1);//the top right corner of the box
+				revoluteJointDef.localAnchorB.Set(-1,0);
+  				b2RevoluteJoint* m_joint = (b2RevoluteJoint*)m_world->CreateJoint( &revoluteJointDef );
+		
+
+
 			}
+
    		}
 		
         void Step(Settings* settings)
@@ -41,6 +68,7 @@
             //show some text in the main screen
             m_debugDraw.DrawString(5, m_textLine, "Now we have a foo test");
             m_textLine += 15;
+
         }
     
         static Test* Create()
